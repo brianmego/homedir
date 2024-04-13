5ef84bd2c3f6f0f899132080f468b98f82fe45a7
Brian Mego <brianmego@gmail.com>
2024-04-13 16:28:38 -0500

    Inlay Hint support

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,3 +1,4 @@
+require("neodev").setup({})
 -- Setup language servers.
 local lspconfig = require('lspconfig')
 lspconfig.lua_ls.setup{}
@@ -23,12 +24,14 @@ vim.keymap.set('n', '<leader>l', vim.diagnostic.setloclist)
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('UserLspConfig', {}),
   callback = function(ev)
+    local opts = { buffer = ev.buf }
     -- Enable completion triggered by <c-x><c-o>
-    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+    vim.bo[opts.buffer].omnifunc = 'v:lua.vim.lsp.omnifunc'
+    vim.lsp.inlay_hint.enable(opts.buffer, true)
 
     -- Buffer local mappings.
     -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local opts = { buffer = ev.buf }
+    -- vim.api.nvim_set_hl(opts.buffer, "LspinlayHint", { fg="grey", })
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
@@ -41,6 +44,9 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end, opts)
     vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
     vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', '<leader>h', function()
+        vim.lsp.inlay_hint.enable(opts.buffer, not vim.lsp.inlay_hint.is_enabled())
+    end, opts)
     vim.keymap.set({ 'n', 'v' }, '<leader>a', vim.lsp.buf.code_action, opts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
     vim.keymap.set('n', '<leader>f', function()
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -86,6 +86,7 @@ return {
             require("mason").setup()
         end,
     },
+    {'folke/neodev.nvim', opts = {}},
     {
         'neovim/nvim-lspconfig',
         config = function()
